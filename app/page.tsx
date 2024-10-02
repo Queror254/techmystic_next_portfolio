2364bf79b0d87197345884fa7e9e66971b986ece
Queror254 <mwendavictorm@gmail.com>
2024-10-02 16:52:19 +0300

    skills_sec v1

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Contact from "@/components/contact";
 export default function Home() {
   return (
     <main className="h-auto w-full rounded-lg bg-background md:px-10 px-1">
-        <div className="body_container flex flex-col font-normal overflow-y-hidden">
+        <div className="body_container w-full flex flex-col font-normal overflow-y-hidden px-0 m-auto">
           <TracingBeam>
           <Hero />
           <Skills />
